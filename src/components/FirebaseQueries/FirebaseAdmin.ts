673f2284418641e7e108a43ea8e012f1090e974b
Nohaila <abouhachemayoub@gmail.com>
2025-06-05 01:17:42 +0100

    Very minor adjustment

--- src/components/FirebaseQueries/FirebaseAdmin.ts
+++ src/components/FirebaseQueries/FirebaseAdmin.ts
@@ -4,7 +4,7 @@ if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.PROJECT_ID,
-      clientEmail: process.env.EMAIL_SENDER,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
   });
